8b0c453004b3fce642542a2b7e12e4fa6d508f25
Carlos V <carlosv.provoos@gmail.com>
2025-02-12 18:15:54 -0500

    add nodo modules new dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,9 @@
+import * as _elizaos_core from '@elizaos/core';
+
 declare const twitterPlugin: {
     name: string;
     description: string;
-    clients: Client[];
+    clients: _elizaos_core.Client[];
 };
 
 export { twitterPlugin as default };
